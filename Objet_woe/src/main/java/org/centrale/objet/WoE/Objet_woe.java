fcfa949d65af6cf1c88eea593573d535240a52e8
RBoutroux <remibtrx@gmail.com>
2024-09-20 10:03:11 +0200

    Ajout de tous les fichiers .java du projet

---
+++ Objet_woe/src/main/java/org/centrale/objet/WoE/Objet_woe.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author remib
+ */
+public class Objet_woe {
+
+    public static void main(String[] args) {
+        World world = new World();
+        world.creerMondeAlea();
+        world.afficheWorld();
+    }
+}
