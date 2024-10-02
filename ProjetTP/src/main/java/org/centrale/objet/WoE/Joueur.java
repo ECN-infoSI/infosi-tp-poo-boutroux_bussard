af668ac2f6bb6d21f6e8370cd1c14e6827f1caa5
RBoutroux <remibtrx@gmail.com>
2024-10-02 19:01:09 +0200

    [feat] Creation interface deplacable et classe joueur

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Joueur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author remib
+ */
+public class Joueur {
+    public Personnage perso; 
+
+    public Joueur() {
+    }
+
+    public Joueur(Personnage perso) {
+        this.perso = perso;
+    }
+}
