7a76e67c3c3c8fd495235bc66b13a048a3801642
Quentin.Bussard <quentin.bussard@eleves.ec-nantes.fr>
2024-10-04 10:40:30 +0200

    [feat] add nomenclature monstre et objets

--- SQL/query.sql
+++ SQL/query.sql
@@ -2,6 +2,12 @@
 I want to write in pl/pgsql
 */
 
-INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+-- INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+TRUNCATE creature,humanoide,type_monstre,type_objet,monde,inventaire,joueur,monstre,objet,partie,sauvegarde,type_humanoide;
 
-INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+
+INSERT INTO type_objet VALUES ('Epee'),('PotionSoin');
+INSERT INTO type_monstre VALUES ('Lapin'),('Loup');
+
+
+
