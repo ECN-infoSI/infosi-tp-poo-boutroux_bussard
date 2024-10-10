72373e3f7d9ade33a4c96644b0e0e467a722be65
Quentin.Bussard <quentin.bussard@eleves.ec-nantes.fr>
2024-10-10 08:56:58 +0200

    [feat] add trolls golbelins massue and add sexe

--- SQL/query.sql
+++ SQL/query.sql
@@ -11,13 +11,15 @@ I want to write in pl/pgsql
 
 TRUNCATE creature,humanoide,nomenclature_monstre,nomenclature_objet,monde,inventaire,joueur,monstre,objet,partie,sauvegarde,nomenclature_humanoide;
 
-INSERT INTO nomenclature_objet VALUES ('Epee'),('PotionSoin');
+INSERT INTO nomenclature_objet VALUES ('Epee'),('PotionSoin'),('Massue');
 
-INSERT INTO nomenclature_monstre VALUES ('Lapin'),('Loup');
+INSERT INTO nomenclature_monstre VALUES ('Lapin'),('Loup'),('trolls'),('gobelin');
 
 INSERT INTO nomenclature_humanoide (type_humanoide) VALUES ('Guerrier'), ('Archer'), ('Paysan');
 
-INSERT INTO monde (id_monde, largeur, longueur, id_creature) VALUES ('m1', 50, 50, NULL), ('m2', 55, 100, NULL), ('m3', 60, 10, NULL), ('m4', 20, 50, NULL), ('m5', 50, 50, NULL);
+INSERT INTO monde (id_monde, largeur, longueur) 
+VALUES ('m1', 50, 50), ('m2', 55, 100),
+('m3', 60, 10), ('m4', 20, 50), ('m5', 50, 50);
 
 INSERT INTO inventaire (id_inventaire) VALUES ('i2'), ('i3'), ('i4');
 
@@ -38,22 +40,22 @@ VALUES ('m1',20,70,8,'Loup'),('m2',20,70,8,'Loup'),('m3',8,15,2,'Lapin'),('m4',8
 INSERT INTO sauvegarde (id_monde,id_partie,date_sauvegarde,nom) 
 VALUES ('m1','p1','2024-04-10 00:00:00','Sauvegarde_1'),('m2','p1','2024-04-10 03:00:00','Sauvegarde_2'),('m5','p3','2024-04-10 00:00:00','Sauvegarde_1');
 
-INSERT INTO creature (id_creature,point_de_vie,est_le_joueur,position_x,position_y,id_monde,id_humanoide,id_monstre,id_inventaire) VALUES 
+INSERT INTO creature (id_creature,point_de_vie,position_x,position_y,id_monde,id_humanoide,id_monstre,id_inventaire,est_male) VALUES 
 -- monde 1
-('c1',100,10,10,'m1','g1',NULL,NULL), ('c2',100,10,10,'m1',NULL,'m1',NULL), ('c3',100,10,10,'m1',NULL,'m2',NULL), 
+('c1',100,10,10,'m1','g1',NULL,NULL,TRUE), ('c2',100,10,10,'m1',NULL,'m1',NULL,FALSE), ('c3',100,10,10,'m1',NULL,'m2',NULL,FALSE), 
 -- monde 2
-('c4',100,10,10,'m2',NULL,'m3',NULL), ('c5',100,10,10,'m2','g2',NULL,'i2'), 
+('c4',100,10,10,'m2',NULL,'m3',NULL,FALSE), ('c5',100,10,10,'m2','g2',NULL,'i2',TRUE), 
 -- monde 3
-('c6',100,10,10,'m3','a1',NULL,NULL), ('c7',100,10,10,'m3','g3',NULL,NULL), ('c8',100,10,10,'m3','g4',NULL,NULL), ('c9',100,10,10,'m3',NULL,'m4',NULL), 
+('c6',100,10,10,'m3','a1',NULL,NULL,FALSE), ('c7',100,10,10,'m3','g3',NULL,NULL,TRUE), ('c8',100,10,10,'m3','g4',NULL,NULL,FALSE), ('c9',100,10,10,'m3',NULL,'m4',NULL,TRUE), 
 --monde 4
-('c10',100,10,10,'m4',NULL,'m5',NULL), ('c11',100,10,10,'m4',NULL,'m6',NULL), ('c12',100,10,10,'m4',NULL,'m7',NULL), ('c13',100,10,10,'m4','a2',NULL,NULL), ('c14',100,10,10,'m4','g5',NULL,NULL),
+('c10',100,10,10,'m4',NULL,'m5',NULL,TRUE), ('c11',100,10,10,'m4',NULL,'m6',NULL,FALSE), ('c12',100,10,10,'m4',NULL,'m7',NULL,FALSE), ('c13',100,10,10,'m4','a2',NULL,NULL,FALSE), ('c14',100,10,10,'m4','g5',NULL,NULL,FALSE),
 -- monde 5
-('c15',100,10,10,'m5','g6',NULL,'i3'), ('c16',100,10,10,'m5',NULL,'m8','i4');
+('c15',100,10,10,'m5','g6',NULL,'i3',TRUE), ('c16',100,10,10,'m5',NULL,'m8','i4',TRUE);
 
 
 UPDATE partie SET date_derniere_sauvegarde_rapide='2024-04-01 00:00:10', id_monde_sauvegarde_rapide='m4'
 WHERE id_partie='p2';
 
-UPDATE monde SET id_creature='c1' WHERE id_monde='m2';
-UPDATE monde SET id_creature='c14' WHERE id_monde='m4';
-UPDATE monde SET id_creature='c16' WHERE id_monde='m5';
+UPDATE monde SET id_creature='c1',nom_personnage='Conan' WHERE id_monde='m2';
+UPDATE monde SET id_creature='c14',nom_personnage='Gurdil' WHERE id_monde='m4';
+UPDATE monde SET id_creature='c16',nom_personnage='Jojo' WHERE id_monde='m5';
