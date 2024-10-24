f6b31dac2da9fa7cc3a0b7a750a154a0f8b61765
RBoutroux <remibtrx@gmail.com>
2024-10-24 09:35:23 +0200

    [feat] creation saegusa et nomenclatures query

---
+++ SQL/creation_saegusa_joueur.sql
@@ -0,0 +1,20 @@
+/*
+I want to write in pl/pgsql
+*/
+
+-- This is a comment
+
+-- delete all rows
+-- DELETE FROM table_name;
+
+-- INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+
+TRUNCATE creature,humanoide,nomenclature_monstre,nomenclature_objet,monde,inventaire,joueur,monstre,objet,partie,sauvegarde,nomenclature_humanoide;
+
+INSERT INTO nomenclature_objet VALUES ('Epee'),('PotionSoin'),('Massue');
+
+INSERT INTO nomenclature_monstre VALUES ('Lapin'),('Loup'),('trolls'),('gobelin');
+
+INSERT INTO nomenclature_humanoide (type_humanoide) VALUES ('Guerrier'), ('Archer'), ('Paysan');
+
+INSERT INTO joueur (nom_de_code, mot_de_passe) VALUES ('Saegusa', 'Mayumi');
